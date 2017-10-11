930511935acb191a510b8cd2fc92798d34aef1f0
Joshua Pace <joshpace@gmail.com>
2017-10-10 20:00:37 -0700

    [add] https

--- server/server.js
+++ server/server.js
@@ -4,6 +4,9 @@ const express = require('express')
 const app = express()
 const routes = require('./api/routes.main')
 const PORT = process.env.PORT || 8000
+const fs = require('fs')
+const https = require('https')
+const HTTPS_PORT = 1443
 
 // Middleware
 require('./config/middleware')(app, express)
@@ -11,6 +14,15 @@ require('./config/middleware')(app, express)
 app.get('/', (req, res) => {
   console.log('Cookies: ', req.cookies)
 })
+
+// HTTPS
+let secureServer = https.createServer({
+  key: fs.readFileSync('./server/config/private.key'),
+  cert: fs.readFileSync('./server/config/certificate.pem')
+}, app)
+  .listen(HTTPS_PORT, () => {
+    console.log('Secure Server listening on port: ' + HTTPS_PORT)
+  })
 
 // Routes
 app.use('/api', routes)
