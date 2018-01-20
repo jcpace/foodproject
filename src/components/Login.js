4736218b1276ea166f8212fe6ee8edc0a8703659
Joshua Pace <joshpace@gmail.com>
2018-01-20 12:43:20 -0800

    login2

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,82 +1,62 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
-import { connect } from 'react-redux'
+import React, { Component } from "react";
+import { Button, FormGroup, FormControl, ControlLabel } from "react-bootstrap";
+import "./Login.css";
 
-import { userActions } from '../_actions'
-
-class LoginPage extends React.Component {
-  constructor (props) {
-    super(props)
-
-        // reset login status
-    this.props.dispatch(userActions.logout())
+export default class Login extends Component {
+  constructor(props) {
+    super(props);
 
     this.state = {
-      username: '',
-      password: '',
-      submitted: false
-    }
-
-    this.handleChange = this.handleChange.bind(this)
-    this.handleSubmit = this.handleSubmit.bind(this)
+      email: "",
+      password: ""
+    };
   }
 
-  handleChange (e) {
-    const { name, value } = e.target
-    this.setState({ [name]: value })
+  validateForm() {
+    return this.state.email.length > 0 && this.state.password.length > 0;
   }
 
-  handleSubmit (e) {
-    e.preventDefault()
-
-    this.setState({ submitted: true })
-    const { username, password } = this.state
-    const { dispatch } = this.props
-    if (username && password) {
-      dispatch(userActions.login(username, password))
-    }
+  handleChange = event => {
+    this.setState({
+      [event.target.id]: event.target.value
+    });
   }
 
-  render () {
-    const { loggingIn } = this.props
-    const { username, password, submitted } = this.state
+  handleSubmit = event => {
+    event.preventDefault();
+  }
+
+  render() {
     return (
-      <div className='col-md-6 col-md-offset-3'>
-        <h2>Login</h2>
-        <form name='form' onSubmit={this.handleSubmit}>
-          <div className={'form-group' + (submitted && !username ? ' has-error' : '')}>
-            <label htmlFor='username'>Username</label>
-            <input type='text' className='form-control' name='username' value={username} onChange={this.handleChange} />
-            {submitted && !username &&
-            <div className='help-block'>Username is required</div>
-                        }
-          </div>
-          <div className={'form-group' + (submitted && !password ? ' has-error' : '')}>
-            <label htmlFor='password'>Password</label>
-            <input type='password' className='form-control' name='password' value={password} onChange={this.handleChange} />
-            {submitted && !password &&
-            <div className='help-block'>Password is required</div>
-                        }
-          </div>
-          <div className='form-group'>
-            <button className='btn btn-primary'>Login</button>
-            {loggingIn &&
-            <img src='data:image/gif;base64,R0lGODlhEAAQAPIAAP///wAAAMLCwkJCQgAAAGJiYoKCgpKSkiH/C05FVFNDQVBFMi4wAwEAAAAh/hpDcmVhdGVkIHdpdGggYWpheGxvYWQuaW5mbwAh+QQJCgAAACwAAAAAEAAQAAADMwi63P4wyklrE2MIOggZnAdOmGYJRbExwroUmcG2LmDEwnHQLVsYOd2mBzkYDAdKa+dIAAAh+QQJCgAAACwAAAAAEAAQAAADNAi63P5OjCEgG4QMu7DmikRxQlFUYDEZIGBMRVsaqHwctXXf7WEYB4Ag1xjihkMZsiUkKhIAIfkECQoAAAAsAAAAABAAEAAAAzYIujIjK8pByJDMlFYvBoVjHA70GU7xSUJhmKtwHPAKzLO9HMaoKwJZ7Rf8AYPDDzKpZBqfvwQAIfkECQoAAAAsAAAAABAAEAAAAzMIumIlK8oyhpHsnFZfhYumCYUhDAQxRIdhHBGqRoKw0R8DYlJd8z0fMDgsGo/IpHI5TAAAIfkECQoAAAAsAAAAABAAEAAAAzIIunInK0rnZBTwGPNMgQwmdsNgXGJUlIWEuR5oWUIpz8pAEAMe6TwfwyYsGo/IpFKSAAAh+QQJCgAAACwAAAAAEAAQAAADMwi6IMKQORfjdOe82p4wGccc4CEuQradylesojEMBgsUc2G7sDX3lQGBMLAJibufbSlKAAAh+QQJCgAAACwAAAAAEAAQAAADMgi63P7wCRHZnFVdmgHu2nFwlWCI3WGc3TSWhUFGxTAUkGCbtgENBMJAEJsxgMLWzpEAACH5BAkKAAAALAAAAAAQABAAAAMyCLrc/jDKSatlQtScKdceCAjDII7HcQ4EMTCpyrCuUBjCYRgHVtqlAiB1YhiCnlsRkAAAOwAAAAAAAAAAAA==' />
-                        }
-            <Link to='/register' className='btn btn-link'>Register</Link>
-          </div>
+      <div className="Login">
+        <form onSubmit={this.handleSubmit}>
+          <FormGroup controlId="email" bsSize="large">
+            <ControlLabel>Email</ControlLabel>
+            <FormControl
+              autoFocus
+              type="email"
+              value={this.state.email}
+              onChange={this.handleChange}
+            />
+          </FormGroup>
+          <FormGroup controlId="password" bsSize="large">
+            <ControlLabel>Password</ControlLabel>
+            <FormControl
+              value={this.state.password}
+              onChange={this.handleChange}
+              type="password"
+            />
+          </FormGroup>
+          <Button
+            block
+            bsSize="large"
+            disabled={!this.validateForm()}
+            type="submit"
+          >
+            Login
+          </Button>
         </form>
       </div>
-    )
+    );
   }
 }
-
-function mapStateToProps (state) {
-  const { loggingIn } = state.authentication
-  return {
-    loggingIn
-  }
-}
-
-const connectedLoginPage = connect(mapStateToProps)(LoginPage)
-export { connectedLoginPage as LoginPage }
