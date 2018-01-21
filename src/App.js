3800708aeca80e83e7ef03afb34ab8a0ee1b0deb
Joshua Pace <joshpace@gmail.com>
2018-01-20 21:11:52 -0800

    rm RR

--- src/App.js
+++ src/App.js
@@ -1,36 +1,28 @@
 import React, { Component } from 'react'
-import { Router, Route, Switch } from 'react-router'
 import './App.scss'
 import Login from './components/login/Login'
 
 class App extends Component {
-  constructor () {
-    super()
+  constructor (props) {
+    super(props)
 
     this.state = {
       vendors: [],
       value: ''
+
     }
   }
-  componentDidMount() {
-   
-  }
 
-  handleChange = event => {
-    this.setState({value: event.target.value});
-  }
-  submitSearch = () => {
-    console.log('search')
-  }
   render () {
     return (
+
       <Login />
+
     )
   }
 }
 
 // loadMap(){
-
 
 //       let latLng = new google.maps.LatLng(this.bookInfo.lat, this.bookInfo.lon);
 //       let mapOptions = {
