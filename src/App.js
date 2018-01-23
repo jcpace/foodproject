97d35a1ac3e88c13f5e949e832ce20b2068e5b0f
Joshua Pace <joshpace@gmail.com>
2018-01-23 11:34:19 -0800

    map work

--- src/App.js
+++ src/App.js
@@ -13,21 +13,19 @@ class App extends Component {
   }
 
   render() {
-    return <Login />;
+    return (
+    <Login />;
+    {map}
+    )
   }
 }
 
-// loadMap(){
-
-//       let latLng = new google.maps.LatLng(this.bookInfo.lat, this.bookInfo.lon);
-//       let mapOptions = {
-//         center: latLng,
-//         zoom: 11,6
-//         mapTypeId: google.maps.MapTypeId.ROADMAP
-//       }
-
-//       this.map = new google.maps.Map(this.mapElement.nativeElement, mapOptions);
-
-//     }
+let map;
+initMap = () => {
+        map = new google.maps.Map(document.getElementById('map'), {
+          center: {lat: -34.397, lng: 150.644},
+          zoom: 8
+        });
+      }
 
 export default App;
