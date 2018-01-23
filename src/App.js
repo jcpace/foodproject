31d072db1ea9e1bc8635767b1c6be517d9d63587
Joshua Pace <joshpace@gmail.com>
2018-01-23 11:40:42 -0800

    initmap

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import React, { Component } from 'react';
 import './App.scss';
 import Login from './components/login/Login';
 
+let map;
+
 class App extends Component {
   constructor(props) {
     super(props);
@@ -11,21 +13,21 @@ class App extends Component {
       value: '',
     };
   }
+  initMap() {
+    this.map = new google.maps.Map(document.getElementById('map'), {
+      center: { lat: -34.397, lng: 150.644 },
+      zoom: 8,
+    });
+  }
 
   render() {
     return (
-    <Login />;
-    {map}
-    )
+      <div>
+        <Login />
+        <div>{map}</div>
+      </div>
+    );
   }
 }
-
-let map;
-initMap = () => {
-        map = new google.maps.Map(document.getElementById('map'), {
-          center: {lat: -34.397, lng: 150.644},
-          zoom: 8
-        });
-      }
 
 export default App;
