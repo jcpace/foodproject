8aa0756bf8275a71648d4aae732881394c8cb06e
Joshua Pace <joshpace@gmail.com>
2018-01-23 23:43:10 -0800

    propTypes

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import React, { Component } from 'react';
 import './App.scss';
 import Login from './components/login/Login';
 
+
 class App extends Component {
   constructor(props) {
     super(props);
@@ -14,26 +15,33 @@ class App extends Component {
       map: undefined,
     };
   }
+ 
   componentDidMount() {
     this.initMap();
   }
   initMap = () =>{
     this.setState({
-      map: new google.maps.Map(document.getElementsByClassName('mapp'), {
+      map: new google.maps.Map(document.getElementsByClassName('map'), {
         center: { lat: -34.397, lng: 150.644 },
         zoom: 8,
       }),
     });
   }
 
+
   render() {
     return (
       <div>
         <Login />
-        <div className="mapp">{this.map}</div>
+        <div className="map">{this.map}</div>
       </div>
+      
     );
   }
+}
+
+App.propTypes = {
+  className: React.propTypes.string.isRequired
 }
 
 export default App;
